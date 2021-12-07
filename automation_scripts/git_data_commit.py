c42f3e86a1cb48f6eadeb01ccde56683d6f02f95
Surveyor-Jr <matingonk@gmail.com>
2021-12-07 12:31:33 +0000

    latest-data

--- automation_scripts/git_data_commit.py
+++ automation_scripts/git_data_commit.py
@@ -42,46 +42,7 @@ prov_time_series_df.to_csv(r"C:\Users\DELL\Desktop\GitHub Repositories\Zimbabwe-
 
 print("Data has been successfully updated")
 
-# Commit the file changes to GitHub
-# import base64
-# from github import Github
-# from github import InputGitTreeElement
-
-# user = "matingonk@gmail.com"
-# password = "nkm10636"
-# g = Github(user,password)
-# repo = g.get_user().get_repo('Zimbabwe-COVID-19-Data') # repo name
-# file_list = [
-#     r'C:\Users\DELL\Desktop\GitHub Repositories\Zimbabwe-COVID-19-Data\time_series_data\daily_cumulative_records.csv',
-#     r'C:\Users\DELL\Desktop\GitHub Repositories\Zimbabwe-COVID-19-Data\vaccination_progress\vaccination_metrics.csv',
-#     r'C:\Users\DELL\Desktop\GitHub Repositories\Zimbabwe-COVID-19-Data\Provincial\current_prov_stats.csv',
-#     r'C:\Users\DELL\Desktop\GitHub Repositories\Zimbabwe-COVID-19-Data\time_series_data\daily_provincial_records.csv'
-# ]
-# file_names = [
-#     'daily_cumulative_records.csv',
-#     'vaccination_metrics.csv',
-#     'current_prov_stats.csv',
-#     'daily_provincial_records.csv'
-# ]
-# commit_message = 'latest data update'
-# master_ref = repo.get_git_ref('heads/master')
-# master_sha = master_ref.object.sha
-# base_tree = repo.get_git_tree(master_sha)
-
-# element_list = list()
-# for i, entry in enumerate(file_list):
-#     with open(entry) as input_file:
-#         data = input_file.read()
-#     if entry.endswith('.png'): # images must be encoded
-#         data = base64.b64encode(data)
-#     element = InputGitTreeElement(file_names[i], '100644', 'blob', data)
-#     element_list.append(element)
-
-# tree = repo.create_git_tree(element_list, base_tree)
-# parent = repo.get_git_commit(master_sha)
-# commit = repo.create_git_commit(commit_message, tree, [parent])
-# master_ref.edit(commit.sha)
-
+# Commiting all files to GitHub
 from gitpy import commitAdd, push
 
 commitAdd("latest-data")
